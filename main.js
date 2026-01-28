3d862cd16c6452465ea746ab99788033171f0681
naomie06 <naomiejessica60@gmail.com>
2026-01-28 08:24:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Etudiante en Genie logiciel", "Developpeuse full-stack junior", "Developpeuse d'application", "Programmeuse", "UX/UI designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
